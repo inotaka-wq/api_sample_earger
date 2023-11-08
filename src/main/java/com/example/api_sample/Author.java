09cb784b53da874fb1512c9d415b0fcafa3c2b7f
pino <tkinoue0324@gmail.com>
2023-11-08 21:36:03 +0900

    JOINSQL追加

--- src/main/java/com/example/api_sample/Author.java
+++ src/main/java/com/example/api_sample/Author.java
@@ -1,4 +1,31 @@
 package com.example.api_sample;
 
-public record Author(Integer id, String firstName, String lastName) {
+public class Author {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
--- src/main/java/com/example/api_sample/BookMapper.java
+++ src/main/java/com/example/api_sample/BookMapper.java
@@ -15,10 +15,12 @@ public interface BookMapper {
 	Book getById(Integer id);
 
 	@Results(id = "allbook", value = {
-			@Result(
-				property = "author", 
-				one = @One(select = "com.example.api_sample.AuthorMapper.getById", 
-				fetchType = FetchType.EAGER), column = "authorId") })
-	@Select("SELECT id, name, page_count AS pageCount, author_id AS authorId FROM book")
+			@Result(property = "author", one = @One(select = "com.example.api_sample.AuthorMapper.getById", fetchType = FetchType.EAGER), column = "author_id") })
+	@Select("SELECT id, name, page_count AS pageCount, author_id FROM book")
 	List<Book> findAll();
+
+	@Select("SELECT b.id, b.name, b.page_count,	b.author_id, a.id as \"author.id\",	a.first_name as \"author.first_name\",	a.last_name  as \"author.last_name\" "
+			+ "FROM book b "
+			+ "LEFT OUTER JOIN author a ON b.author_id = a.id")
+	List<Book> findWithAuthors();
 }
